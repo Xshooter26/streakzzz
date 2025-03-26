9559bc8368b433b736dd314c4bdc082a9cfada5e
Xshooter26 <harshpidurkar2003@gmail.com>
2025-03-26 21:55:58 +0530

    build error fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import { Inter } from 'next/font/google'
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+
 import "./globals.css";
 import { ThemeProvider } from "./components/ThemeProvider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-export const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-})
+
+const inter = Inter({ subsets: ['latin'] })
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -24,7 +22,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={inter.className}>
       <body className="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white">
       <ThemeProvider
           attribute="class"
