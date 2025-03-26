d4aeb68e4be7d02012ef614cc20e44a55f27bb62
Xshooter26 <harshpidurkar2003@gmail.com>
2025-03-26 22:02:41 +0530

    Fix font issue in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,37 +3,27 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 
 import "./globals.css";
 import { ThemeProvider } from "./components/ThemeProvider";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata = {
+export const metadata: Metadata = {
   title: "Streakzzz",
   description: "Track your habits and build streaks!",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning className={inter.className}>
+    <html lang="en" suppressHydrationWarning>
       <body className="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white">
-      <ThemeProvider
+        <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-        {children}
+          {children}
         </ThemeProvider>
       </body>
     </html>
   );
 }
-
